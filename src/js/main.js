1fc0e11951a1320406f8d641f889de29c51c9942
SabyGil <sabygil@yahoo.com>
2018-09-19 12:00:30 -0400

    added menu trigger btn to home page

--- src/js/main.js
+++ src/js/main.js
@@ -4,27 +4,30 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
+const menuTrigger = document.querySelector('#menu-trigger');
 
 //set initial state of menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
 
+menuTrigger.addEventListener('click', toggleMenu);
+
 function toggleMenu() {
-  if (!showMenu) {
-    menuBtn.classList.add('close');
-    menu.classList.add('show');
-    menuNav.classList.add('show');
-    menuBranding.classList.add('show');
-    navItems.forEach(item => item.classList.add('show'));
-    //set menu state
-    showMenu = true;
-  } else {
-    menuBtn.classList.remove('close');
-    menu.classList.remove('show');
-    menuNav.classList.remove('show');
-    menuBranding.classList.remove('show');
-    navItems.forEach(item => item.classList.remove('show'));
-    showMenu = false;
-  }
+	if(!showMenu) {
+		menuBtn.classList.add('close');
+		menu.classList.add('show');
+		menuNav.classList.add('show');
+		menuBranding.classList.add('show');
+		navItems.forEach(item => item.classList.add('show'));
+		//set menu state
+		showMenu = true;
+	} else {
+		menuBtn.classList.remove('close');
+		menu.classList.remove('show');
+		menuNav.classList.remove('show');
+		menuBranding.classList.remove('show');
+		navItems.forEach(item => item.classList.remove('show'));
+		showMenu = false;
+	}
 }
